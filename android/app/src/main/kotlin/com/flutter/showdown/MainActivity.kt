c03c852d92b5ccc7ae38786eb290a7746c38b7d9
Adrien Rouhète <adrien@kara.ai>
2020-10-23 17:09:11 +0200

    New icons

--- android/app/src/main/kotlin/com/flutter/showdown/MainActivity.kt
+++ android/app/src/main/kotlin/com/flutter/showdown/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.flutter_project
+package com.flutter.showdown
 
 import io.flutter.embedding.android.FlutterActivity
 
---
+++ lib/pages/team_builder/pokedex/pokemon_details.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_showdown/extensions/string_extension.dart';
+import '../../../models/pokemon.dart';
+
+class PokemonDetails extends StatelessWidget {
+  const PokemonDetails(this.pokemon);
+  final Pokemon pokemon;
+
+  @override
+  Widget build(BuildContext context) {
+    final name = pokemon.name.capitalize();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('$name Details'),
+      ),
+      body: Column(
+        children: [
+          Center(
+            child: Image.network(
+              'https://play.pokemonshowdown.com/sprites/ani/${pokemon.name}.gif',
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
