ceeb4652557ca3c466d5bd759306cd36bf0b67f3
Renaud <renaud.melet@epitech.eu>
2020-10-25 14:20:33 +0100

    Handle player's avatars and ranks

---
+++ lib/parser.dart
@@ -0,0 +1,91 @@
+class Parser {
+  static String toId(String username) {
+    return username.toLowerCase().replaceAll(RegExp(r'[^a-z0-9]+'), '');
+  }
+
+  static List<String> parseRoomMessage(String text) {
+    String roomId = '';
+
+    if (text.startsWith('>')) {
+      final int nlIdx = text.indexOf('\n');
+
+      roomId = text.substring(1, nlIdx);
+      text = text.substring(nlIdx + 1);
+    }
+    return [roomId, text];
+  }
+
+  static List<String> parseLine(String line) {
+    if (!line.startsWith('|')) {
+      return ['', line];
+    } else if (line == '|') {
+      return ['done'];
+    }
+
+    final int index = line.indexOf('|', 1);
+    final String cmd = line.substring(1, index);
+
+    switch (cmd) {
+      case 'chatmsg':
+      case 'chatmsg-raw':
+      case 'raw':
+      case 'error':
+      case 'html':
+      case 'inactive':
+      case 'inactiveoff':
+      case 'warning':
+      case 'fieldhtml':
+      case 'contolshtml':
+      case 'bigerror':
+      case 'debug':
+      case 'tier':
+      case 'challstr':
+      case 'popup':
+      case '':
+        return [cmd, line.substring(index + 1)];
+      case 'c':
+      case 'chat':
+      case 'uhtml':
+      case 'uhtmlchange':
+      case 'queryresponse':
+        final int index2 = line.indexOf('|', index + 1);
+
+        return [
+          cmd,
+          line.substring(index + 1, index2),
+          line.substring(index2 + 1)
+        ];
+      case 'c:':
+      case 'pm':
+        final int index2 = line.indexOf('|', index + 1);
+        final int index3 = line.indexOf('|', index2 + 1);
+
+        return [
+          cmd,
+          line.substring(index + 1, index2),
+          line.substring(index2 + 1, index3),
+          line.substring(index3 + 1)
+        ];
+    }
+    return line.substring(1).split('|');
+  }
+
+  static List<String> parseName(String text) {
+    String group = ' ';
+
+    if (RegExp(r'^[^A-Za-z0-9]').firstMatch(text) != null) {
+      group = text[0];
+      text = text.substring(1);
+    }
+
+    String name = text;
+    String status = '';
+    final int atIndex = name.indexOf('@');
+
+    if (atIndex > 0) {
+      name = text.substring(0, atIndex);
+      status = text.substring(atIndex + 1);
+    }
+    return [name, group, status];
+  }
+}
