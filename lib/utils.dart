b5b104f847ee182a2508c3d9cb9711419244325f
Renaud <renaud.melet@epitech.eu>
2020-11-03 17:42:46 +0100

    Clean constants and utils

--- lib/utils.dart
+++ lib/utils.dart
@@ -29,17 +29,47 @@ Future<List<Image>> splitImages(SplitParameters parameters) async {
 
 String getAvatarLink(String avatar) {
   String link = '';
+  const String AvatarLinkPrefix = ServerUrl + '/sprites/trainers/';
+  const String CustomAvatarLinkPrefix = ServerUrl + '/sprites/trainers-custom/';
 
   if (int.tryParse(avatar) != null) {
-    avatar = BATTLEAVATARNUMBERS[avatar];
+    avatar = BattleAvatarNumbers[avatar];
   }
-  if (AVATARSAVAILABLE.contains(avatar)) {
-    link = AVATARLINKPREFIX + avatar + '.png';
+  if (AvatarsAvailable.contains(avatar)) {
+    link = AvatarLinkPrefix + avatar + '.png';
   } else {
     if (avatar.startsWith('#')) {
       avatar = avatar.substring(1);
     }
-    link = CUSTOMAVATARLINKPREFIX + avatar + '.png';
+    link = CustomAvatarLinkPrefix + avatar + '.png';
   }
   return link;
+}
+
+Text usernameTextColor(String username, String status) {
+  Color color = Colors.grey;
+
+  if (!status.startsWith('!')) {
+    final int modulo = username.length % 5;
+    if (modulo == 0) {
+      color = Colors.lightBlue;
+    } else if (modulo == 1) {
+      color = Colors.orange;
+    } else if (modulo == 2) {
+      color = Colors.pink;
+    } else if (modulo == 3) {
+      color = Colors.lightGreen;
+    } else if (modulo == 4) {
+      color = Colors.deepPurple;
+    }
+  }
+
+  return Text(
+    username,
+    style: TextStyle(
+      color: color,
+      fontFamily: 'Roboto',
+      fontWeight: FontWeight.bold,
+    ),
+  );
 }
