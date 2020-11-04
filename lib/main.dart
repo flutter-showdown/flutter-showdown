b9bdd8e2ba8f880abb24094fcb7796cd20275636
Nyquase <adrien.rouhete@epitech.eu>
2020-11-04 09:53:59 +0100

    Locked screen in portrait mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_showdown/pages/main_screen.dart';
 import 'package:flutter_showdown/startup.dart';
 import 'package:flutter_showdown/pages/login/login_screen.dart';
 
 void main() {
-  runApp(
-    Startup(
-      child: MyApp(),
-    ),
-  );
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) {
+    runApp(
+      Startup(
+        child: MyApp(),
+      ),
+    );
+  });
 }
 
 class MyApp extends StatelessWidget {
