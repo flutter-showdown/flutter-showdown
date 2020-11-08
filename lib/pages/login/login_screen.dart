117d09d6186be55596626f288612f6d5dd121fc7
Lisa Faci <lisa.faci@epitech.eu>
2020-11-08 02:43:54 +0100

    annimation pokemon + change logo/name + fix bottom info affichage

--- lib/pages/login/login_screen.dart
+++ lib/pages/login/login_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter_showdown/pages/login/blue_wave.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_showdown/providers/global_messages.dart';
 import 'login_form.dart';
+import 'moving_pokemon.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -29,52 +30,57 @@ class _LoginScreenState extends State<LoginScreen>
     final user = context.watch<GlobalMessages>().user;
     // final bottom = MediaQuery.of(context).viewInsets.bottom;
 
-    return Scaffold(
-      resizeToAvoidBottomPadding: false,
-      body: Stack(children: [
-        BottomWaveContainer(Container(
-            decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.center,
-                    colors: [Colors.blue, Colors.green])))),
-        LayoutBuilder(builder:
-            (BuildContext context, BoxConstraints viewportConstraints) {
-          return SingleChildScrollView(
-              child: ConstrainedBox(
-                  constraints: BoxConstraints(
-                    minHeight: viewportConstraints.maxHeight,
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Image(
-                        image: AssetImage('assets/icons/pokemon2.png'),
-                        height: 200,
-                        width: 300,
-                        fit: BoxFit.fitWidth,
+    return SafeArea(
+        top: false,
+        bottom: true,
+        child: Scaffold(
+          resizeToAvoidBottomPadding: false,
+          body: Stack(children: [
+            BottomWaveContainer(Container(
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.center,
+                        colors: [Colors.lightBlue[200], Colors.lightBlue])))),
+            PokemonAnimation(),
+            LayoutBuilder(builder:
+                (BuildContext context, BoxConstraints viewportConstraints) {
+              return SingleChildScrollView(
+                  child: ConstrainedBox(
+                      constraints: BoxConstraints(
+                        minHeight: viewportConstraints.maxHeight,
                       ),
-                      if (user == null) const CircularProgressIndicator(),
-                      LoginForm(),
-                    ],
-                  )));
-        }),
-        Positioned(
-            left: 0.0,
-            right: 0.0,
-            bottom: 0.0,
-            child: Center(
-              child: Container(
-                padding: const EdgeInsets.all(6.0),
-                color: Colors.lightBlue[100].withOpacity(0.7),
-                child: const Text(
-                  'All ressources of this application belongs to the website pokemon showdown',
-                  style: TextStyle(fontSize: 10, color: Colors.black),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            )),
-      ]),
-    );
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Image(
+                            image:
+                                AssetImage('assets/icons/showdown-title.png'),
+                            height: 200,
+                            width: 300,
+                            fit: BoxFit.fitWidth,
+                          ),
+                          if (user == null) const CircularProgressIndicator(),
+                          LoginForm(),
+                        ],
+                      )));
+            }),
+            Positioned(
+                left: 0.0,
+                right: 0.0,
+                bottom: 0.0,
+                child: Center(
+                  child: Container(
+                    padding: const EdgeInsets.all(6.0),
+                    color: Colors.lightBlue[100].withOpacity(0.7),
+                    child: const Text(
+                      'All ressources of this application belongs to the website pokemon showdown',
+                      style: TextStyle(fontSize: 10, color: Colors.black),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                )),
+          ]),
+        ));
   }
 }
